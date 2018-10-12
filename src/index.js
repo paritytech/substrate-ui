7f7d5f5e7c5d7400f5e802d2b2a788a7c5138b57
Gav <gavin@parity.io>
2018-10-12 19:55:44 +0200

    Fallback to global RPC server

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import {App} from './app.jsx';
 import { setNodeUri } from 'oo7-substrate'
 require('./denominations')
 
-//setNodeUri('wss://substrate-rpc.parity.io/')
+setNodeUri(['ws://127.0.0.1:9944/', 'wss://substrate-rpc.parity.io/'])
 
 render(<App/>, document.getElementById('app'));
