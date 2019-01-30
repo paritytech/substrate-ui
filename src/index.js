780f6976563f22e5a422b1814678808d4ce3cafc
Gav Wood <gavin@parity.io>
2019-01-30 18:24:00 +0100

    Bump oo7-substrate

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { setNodeUri } from 'oo7-substrate'
 require('./denominations')
 
 //setNodeUri(['ws://127.0.0.1:9944/', 'wss://substrate-rpc.parity.io/', 'wss://poc3-rpc.polkadot.io/', 'ws://104.211.54.233:9944/'])
-setNodeUri(['ws://127.0.0.1:9944/', 'wss://poc3-rpc.polkadot.io/'])
+//setNodeUri(['ws://127.0.0.1:9944/', 'wss://poc3-rpc.polkadot.io/'])
+//setNodeUri(['ws://127.0.0.1:9944/', 'wss://substrate-rpc.parity.io/'])
+//setNodeUri(['wss://substrate-rpc.parity.io/'])
+setNodeUri(['wss://poc3-rpc.polkadot.io/'])
 
 render(<App/>, document.getElementById('app'));
