8e4e6a5f522653b5918efb2454b14769152c9ef8
Gav Wood <gavin@parity.io>
2019-02-01 12:31:23 +0100

    Add endpoints.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ require('./denominations')
 //setNodeUri(['ws://127.0.0.1:9944/', 'wss://poc3-rpc.polkadot.io/'])
 //setNodeUri(['ws://127.0.0.1:9944/', 'wss://substrate-rpc.parity.io/'])
 //setNodeUri(['wss://substrate-rpc.parity.io/'])
-setNodeUri(['wss://poc3-rpc.polkadot.io/'])
+//setNodeUri(['wss://poc3-rpc.polkadot.io/'])
+//setNodeUri(['ws://127.0.0.1:9944/'])
+setNodeUri(['ws://127.0.0.1:9955/'])
 
 render(<App/>, document.getElementById('app'));
